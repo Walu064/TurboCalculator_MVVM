7a09b22cf835bc9879f4492501b95fb1d23d642c
waluwalu064 <waluwalu064@gmail.com>
2022-05-04 08:49:48 +0200

    Utworzenie pustego projektu.

---
+++ app/src/main/java/pl/edu/wat/laborka1asm/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.edu.wat.laborka1asm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
